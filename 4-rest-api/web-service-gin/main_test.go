458f0969fa009ac181b951b6499e96f941f9d797
Ronald Fung <rfung.dev@icloud.com>
2023-11-29 21:18:35 +0000

    Add test TestGetAlbumsReturnsAllRecordsInJSON

---
+++ 4-rest-api/web-service-gin/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetAlbumsReturnsAllRecordsInJSON(t *testing.T) {
+	client := http.Client{}
+	resp, err := client.Get("http://localhost:8080/albums")
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if err != nil {
+		fmt.Printf("Error: %s", err.Error())
+	}
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Contains(t, resp.Header.Get("Content-Type"), "application/json")
+	var receviedData []album
+	err = json.NewDecoder(resp.Body).Decode(&receviedData)
+	if err != nil {
+		t.Fatalf("Error decoding JSON: %v", err)
+	}
+
+	assert.NotEmpty(t, receviedData)
+	assert.Equal(t, receviedData, albums)
+}
